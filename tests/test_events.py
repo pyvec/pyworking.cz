bbff9ab221c7a63aad04de7e88895edbbbc6b685
Petr Messner <petr.messner@gmail.com>
2017-08-27 20:25:09 +0200

    Add tests for load_events and index page

---
+++ tests/test_events.py
@@ -0,0 +1,5 @@
+from pyworking_cz.model import load_events
+
+
+def test_load_events():
+    assert load_events()
--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,2 +1,6 @@
-def test_import():
-    import pyworking_cz
+def test_get_index():
+    from pyworking_cz import app
+    with app.test_client() as c:
+        resp = c.get('/')
+        assert resp.status_code == 200
+        assert b'PyWorking' in resp.data
